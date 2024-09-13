1c4137d639de872df21fe1e26ac84a8eb2eab206
rayanarocha <rayana.azus@gmail.com>
2024-09-13 19:10:49 -0300

    exemplo da temperatura

--- diadasemana2.js
+++ diadasemana2.js
@@ -1,45 +1,38 @@
 const prompt = require ("prompt-sync")();
 
-// let diaSemana = Number(prompt("Insira um valor entre 1 e 7 para selecionar o dia da semana: "));
-// let diaSemanaNome;
+// para receber um número e compará-lo dentro do switch, é preciso dizer ao JS que você espera um número,
+// pois, se você não o fizer, ele vai ler o valor preenchido como string e vai dar erro
 
-//   switch (diaSemana) {
-//     case 1:
-//       diaSemanaNome = "Domingo";
-//       break;
-//     case 2:
-//       diaSemanaNome = "Segunda-feira";
-//       break;
-//     case 3:
-//       diaSemanaNome = "Terça-feira";
-//       break;
-//     case 4:
-//       diaSemanaNome = "Quarta-feira";
-//       break;
-//     case 5:
-//       diaSemanaNome = "Quinta-feira";
-//       break;
-//     case 6:
-//       diaSemanaNome = "Sexta-feira";
-//       break;
-//     case 7:
-//       diaSemanaNome = "Sábado";
-//       break;
-//     default:
-//       diaSemanaNome = "Dia inválido";
-//   }
+let diaSemana = Number(prompt("Insira um valor entre 1 e 7 para selecionar o dia da semana: "));
+let diaSemanaNome;
 
-//   console.log(diaSemanaNome);
+  switch (diaSemana) {
+    case 1:
+      diaSemanaNome = "Domingo";
+      break;
+    case 2:
+      diaSemanaNome = "Segunda-feira";
+      break;
+    case 3:
+      diaSemanaNome = "Terça-feira";
+      break;
+    case 4:
+      diaSemanaNome = "Quarta-feira";
+      break;
+    case 5:
+      diaSemanaNome = "Quinta-feira";
+      break;
+    case 6:
+      diaSemanaNome = "Sexta-feira";
+      break;
+    case 7:
+      diaSemanaNome = "Sábado";
+      break;
+    default:
+      diaSemanaNome = "Dia inválido";
+  }
 
-let temperatura = Number(prompt("Digite uma temperatura: "))
+  console.log(diaSemanaNome);
 
-if(temperatura >= 0 && temperatura < 15){
-  console.log("Está frio!")
-} else if (temperatura > 15 && temperatura <= 25 ) {
-  console.log("Está ameno!")
-} else if (temperatura > 25 && temperatura <= 35) {
-  console.log("Está quente!")
-} else {
-  console.log("Está pegando fogo!")
-}
+
 
---
+++ temperatura.js
@@ -0,0 +1,18 @@
+
+// neste exemplo, verificamos a temperatura dentro de alguns intervalos
+// >= 0 && <= 15 : frio
+// > 15 && <= 25 : ameno
+// > 25 && <= 35 : quente
+// > 35 : pegando fogo
+
+let temperatura = Number(prompt("Digite uma temperatura: "))
+
+if(temperatura >= 0 && temperatura <= 15){
+  console.log("Está frio!")
+} else if (temperatura > 15 && temperatura <= 25 ) {
+  console.log("Está ameno!")
+} else if (temperatura > 25 && temperatura <= 35) {
+  console.log("Está quente!")
+} else {
+  console.log("Está pegando fogo!")
+}
